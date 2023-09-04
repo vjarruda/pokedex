3d25ff2a8f4032c1babd991a9133c4ced94fe267
vjarruda <arrudavini25@gmail.com>
2023-09-04 14:54:43 -0300

    separando codigo da api e diminuindo codigo mainjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-const offset = 0;
-const limit = 10;
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -19,20 +14,11 @@ function convertPokemonToLi(pokemon) {
                     alt="${pokemon.name}">
             </div>
         </li>
-    `    
-}   
+    `
+}
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
