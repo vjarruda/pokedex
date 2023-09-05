7400c0bf4add738f2d6076f94293f7c15d288f26
vjarruda <arrudavini25@gmail.com>
2023-09-05 14:46:53 -0300

    criando paginação para a pokedex

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
             .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
